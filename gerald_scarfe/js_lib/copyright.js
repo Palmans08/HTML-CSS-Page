705036f47318f2169226a73893f438730e32f8fd
Palmans08 <119600505+Palmans08@users.noreply.github.com>
2023-04-08 22:36:54 +0200

    Add files via upload

---
+++ gerald_scarfe/js_lib/copyright.js
@@ -0,0 +1,2 @@
+var y = new Date().getFullYear();
+document.getElementById("copyright").innerHTML = "&copy; " + y + " Created by David Palmans";
---
+++ gerald_scarfe/js_lib/mask_mail.js
@@ -0,0 +1,3 @@
+emailE = 'so.antwerpen.be';
+emailE = ('encora' + '@' + emailE);
+document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>');
